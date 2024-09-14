24a8f9d4ba9ed039904f5d9bbd548b5fcf9f6193
Jhoão <jhpdrnsantos2@gmail.com>
2024-09-13 21:08:42 -0300

    feat: Alteração na cor da páginação

--- src/components/ProjetosSwiper.tsx
+++ src/components/ProjetosSwiper.tsx
@@ -2,30 +2,48 @@ import { MultiActionAreaCard } from "./MultiActionAreaCard.tsx";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import { projetosConfiguracao } from './util/ConfigProjetosSwipper.ts';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
 
 export function ProjetosSwiper() {
     return (
+        <div>
         <Swiper
-            spaceBetween={50}
-            pagination={{ clickable: true }}
-            breakpoints={{
-                640: { slidesPerView: 2, spaceBetween: 20 },
-                768: { slidesPerView: 3, spaceBetween: 40 },
-            }}
-            modules={[Pagination]}
-            className="mySwiper"
+          spaceBetween={50}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            640: { slidesPerView: 2, spaceBetween: 20 },
+            768: { slidesPerView: 3, spaceBetween: 40 },
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+          style={{ margin: 10 }}
         >
-            {projetosConfiguracao.map((projeto, index) => (
-                <SwiperSlide key={index}>
-                    <MultiActionAreaCard
-                        imagemProjeto={projeto.imagem}
-                        nomeProjeto={projeto.nome}
-                        tags={projeto.tags}
-                        link={projeto.link}
-                        descricaoProjeto={projeto.descricao}
-                    />
-                </SwiperSlide>
-            ))}
+          {projetosConfiguracao.map((projeto, index) => (
+            <SwiperSlide key={index}>
+              <MultiActionAreaCard
+                imagemProjeto={projeto.imagem}
+                nomeProjeto={projeto.nome}
+                tags={projeto.tags}
+                link={projeto.link}
+                descricaoProjeto={projeto.descricao}
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
+        <style>
+          {`
+            .swiper-pagination-bullet {
+              background-color: #ccc;
+              opacity: 1;
+            }
+            .swiper-pagination-bullet-active {
+              background-color: #ffa800;
+            }
+          `}
+        </style>
+      </div>
     );
 }
