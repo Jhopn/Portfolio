a203fa5d555c55715face9cb293bdfa4fd782fdb
Jhoão <jhpdrnsantos2@gmail.com>
2024-11-16 20:50:36 -0300

    feat: mudança em algumas estilizações

--- src/components/util/ConfigProjetosSwipper.ts
+++ src/components/util/ConfigProjetosSwipper.ts
@@ -1,8 +1,8 @@
 import imagemTaskApp from '../../assets/Projetos/TaskApp/taskapp-home.webp';
 import imagemTimhontel from '../../assets/Projetos/Timhontel/timhontel-home.webp';
 import imagemMiniGameCalculator from '../../assets/Projetos/MiniGameCalculator/minigamecalculator.webp';
-// import imagemMiniGameCalculator2 from '../../assets/Projetos/MiniGameCalculator/minigamecalculator-dark-mode.webp';
-import imagemMiniGameCalculator3 from '../../assets/Projetos/MiniGameCalculator/minigamecalculator.webp';
+import imagemMiniGameCalculator2 from '../../assets/Projetos/MiniGameCalculator/minigamecalculator-dark-mode.webp';
+import imagemMiniGameCalculator3 from '../../assets/Projetos/MiniGameCalculator/minigamecalculator-rank.webp';
 import imagemApiPlanner from '../../assets/Projetos/ApiPlanner/api-planner.webp';
 import imagemSlideEcommerceExpress from '../../assets/Projetos/EcommerceApi/ecommerceExpress.webp';
 import imagemCardEcommerceNest from '../../assets/Projetos/E-commerce/projetoEcommerce.webp';
@@ -72,7 +72,7 @@ export const projetosConfiguracao = [
         linkSite: ''
     },
     {
-        imagem: ['https://i.im.ge/2024/10/17/kAiMCP.1.png',imagemMiniGameCalculator, imagemMiniGameCalculator3],
+        imagem: ['https://i.im.ge/2024/10/17/kAiMCP.1.png',imagemMiniGameCalculator, imagemMiniGameCalculator2, imagemMiniGameCalculator3],
         nome: 'MiniGameCalculator',
         tags: [
             { nome: 'React Native', cor: '#05d2fe' },
